01f1fec3d85cfd9dbdaa87a7a2e856e17370886b
Ryan-Willpower <surasak.c.work@gmail.com>
2020-09-28 14:28:11 +0700

    init: first time upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [fruits, setFruit] = useState([
+    "apple",
+    "kiwi",
+    "orange",
+    "passion fruit",
+  ]);
+  const [input, setInput] = useState("");
+
+  function addFruitToTopOne() {
+    setFruit([input, ...fruits]);
+    setInput("");
+  }
+
+  function addFruitToLastOne() {
+    setFruit([...fruits, input]);
+    setInput("");
+  }
+
+  function remove(i: number) {
+    fruits.splice(i, 1);
+
+    setFruit([...fruits]);
+  }
+
+  function sortByAlphabet() {
+    setFruit([...fruits].sort());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {fruits.map((fruit, index) => (
+        <div key={index}>
+          <input type="checkbox" /> {index} : {fruit}
+        </div>
+      ))}
+
+      <input onChange={(e) => setInput(e.target.value)} value={input} />
+      <button onClick={() => addFruitToTopOne()}>Add fruit to top</button>
+      <button onClick={() => addFruitToLastOne()}>Add fruit to last</button>
+      <button onClick={() => sortByAlphabet()}>Sort by alphabet</button>
     </div>
   );
 }
